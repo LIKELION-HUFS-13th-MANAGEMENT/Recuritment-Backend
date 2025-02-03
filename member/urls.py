0b98814439697676165bd95729265c27ac20d738
woodzverse <woodzverse@naver.com>
2025-02-03 13:30:33 +0900

    feature: 유저 정보 조회 기능 개발

--- member/urls.py
+++ member/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SignUpView, LoginView, LogoutView
+from .views import SignUpView, LoginView, LogoutView, UserInfoView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("info/", UserInfoView.as_view(), name="info"),
 ]
